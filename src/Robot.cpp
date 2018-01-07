24a45910c9174878234dbfd43a81f64fa256a52a
cgiger00 <cgiger00@gmail.com>
2018-01-07 15:22:56 -0500

    new branch for DIO testing for mechanical

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -3,21 +3,43 @@
 #include <string>
 #include <Lib830.h>
 #include <WPILib.h>
+#include <input/Toggle.h>
+#include <input/GamepadF310.h>
 
 #include <IterativeRobot.h>
 #include <LiveWindow/LiveWindow.h>
 #include <SmartDashboard/SendableChooser.h>
 #include <SmartDashboard/SmartDashboard.h>
 
+//button a is 0
+//button b is 1
+//button x is 2
+//button y is 3
+//D-pad up is 4
+//D-pad down is 5
+
 class Robot: public frc::IterativeRobot {
 public:
-	static const int LEFT_FRONT_PWM = 0; //these numbers are subject to change
-	static const int LEFT_BACK_PWM = 1;
-	static const int RIGHT_FRONT_PWM = 2;
-	static const int RIGHT_BACK_PWM = 3;
 
+	static const int DIO_0 = 0;
+	static const int DIO_1 = 1;
+	static const int DIO_2 = 2;
+	static const int DIO_3 = 3;
+	static const int DIO_4 = 4;
+	static const int DIO_5 = 5;
 	//declare a drive train object & a Lib830:: gamepad object 
 	//object declaration example: Motor *motor
+
+	DigitalOutput *DIO0;
+	DigitalOutput *DIO1;
+	DigitalOutput *DIO2;
+	DigitalOutput *DIO3;
+	DigitalOutput *DIO4;
+	DigitalOutput *DIO5;
+
+
+	Lib830::GamepadF310 *pilot;
+	RobotDrive *drivetrain;
 
 
 	void RobotInit() {
@@ -28,6 +50,14 @@ public:
 		/*initialize drive train (look at http://first.wpi.edu/FRC/roborio/release/docs/cpp/classfrc_1_1RobotDrive.html
 		for constructor */
 		//ex: motor = new Motor (new VictorSP(MOTOR_PWM));
+
+		DIO0 = new DigitalOutput(DIO_0);
+		DIO1 = new DigitalOutput(DIO_1);
+		DIO2 = new DigitalOutput(DIO_2);
+		DIO3 = new DigitalOutput(DIO_3);
+		DIO4 = new DigitalOutput(DIO_4);
+		DIO5 = new DigitalOutput(DIO_5);
+
 
 		//initalize game pad, constructor is Lib830::Gamepad(0)
 		pilot = new Lib830::GamepadF310(0);
@@ -45,40 +75,32 @@ public:
 	 * SendableChooser make sure to add them to the chooser code above as well.
 	 */
 	void AutonomousInit() override {
-		autoSelected = chooser.GetSelected();
-		// std::string autoSelected = SmartDashboard::GetString("Auto Selector", autoNameDefault);
-		std::cout << "Auto selected: " << autoSelected << std::endl;
 
-		if (autoSelected == autoNameCustom) {
-			// Custom Auto goes here
-		} else {
-			// Default Auto goes here
-		}
 	}
 
 	void AutonomousPeriodic() {
-		if (autoSelected == autoNameCustom) {
-			// Custom Auto goes here
-		} else {
-			// Default Auto goes here
-		}
+
 	}
 	void TeleopInit() {
 
 	}
-	float prev_speed = 0;
-	float prev_turn = 0;
+	Toggle dio_0;
+	Toggle dio_1;
+	Toggle dio_2;
+	Toggle dio_3;
+	Toggle dio_4;
+	Toggle dio_5;
+
 
 	void TeleopPeriodic() {
-		
-		float turn = /*from gamepad class, LeftX(),LeftY(),RightX(),RightY() correspond to game pad controlls
-		call function with gamepadobject -> Function() */
-			
-		float speed = /*different axis than speed!*/
-		
-		//use arcade drive function from RobotDrive class  (so you need to do class->ArcadeDrive(speed, turn, false) )
- 		
-		//accel function after basic drive train works 
+
+		DIO0->Set(dio_0.toggle(pilot->ButtonState(Lib830::GamepadF310::BUTTON_A)));
+		DIO1->Set(dio_1.toggle(pilot->ButtonState(Lib830::GamepadF310::BUTTON_B)));
+		DIO2->Set(dio_2.toggle(pilot->ButtonState(Lib830::GamepadF310::BUTTON_X)));
+		DIO3->Set(dio_3.toggle(pilot->ButtonState(Lib830::GamepadF310::BUTTON_Y)));
+		DIO4->Set(dio_4.toggle(pilot->DPadUp()));
+		DIO5->Set(dio_5.toggle(pilot->DPadDown()));
+
 
 	}
 
